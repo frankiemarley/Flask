6936125696b0cd52193a067521e428513ffb1d4b
HTZ\paps_ <paps_1995@hotmail.com>
2024-09-08 18:11:06 +0200

    4

--- src/app.py
+++ src/app.py
@@ -6,25 +6,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from flask import Flask, request, jsonify, render_template
 import joblib
+import os
 
 app = Flask(__name__)
 
-# Load and prepare data
-@app.before_first_request
+# Global variable to hold our model
+model = None
+
 def load_model():
     global model
-    data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv")
-    X = data[['age', 'bmi', 'children', 'smoker', 'region']]
-    y = data['charges']
-    X = pd.get_dummies(X, drop_first=True)
+    if model is None:
+        data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/linear-regression-project-tutorial/main/medical_insurance_cost.csv")
+        X = data[['age', 'bmi', 'children', 'smoker', 'region']]
+        y = data['charges']
+        X = pd.get_dummies(X, drop_first=True)
 
-    # Train the model
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+        # Train the model
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = LinearRegression()
+        model.fit(X_train, y_train)
 
-    # Save the model
-    joblib.dump(model, 'insurance_cost_model.joblib')
+        # Save the model
+        joblib.dump(model, 'insurance_cost_model.joblib')
+
+@app.before_request
+def before_request():
+    load_model()
 
 @app.route('/')
 def home():
@@ -45,4 +52,5 @@ def predict():
     return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
